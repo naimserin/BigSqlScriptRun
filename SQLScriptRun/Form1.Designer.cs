f3de41d214fcedaf96313a3be099528fa73f6955
Naim <naimserin@gmail.com>
2015-11-04 13:26:28 +0200

    Start

---
+++ SQLScriptRun/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace SQLScriptRun
+{
+    partial class frm1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtDosya = new System.Windows.Forms.TextBox();
+            this.btnSec = new System.Windows.Forms.Button();
+            this.btnAktar = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.txtServer = new System.Windows.Forms.TextBox();
+            this.txtCatalog = new System.Windows.Forms.TextBox();
+            this.txtUserID = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtDosya
+            // 
+            this.txtDosya.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtDosya.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.txtDosya.Location = new System.Drawing.Point(21, 40);
+            this.txtDosya.Name = "txtDosya";
+            this.txtDosya.Size = new System.Drawing.Size(516, 31);
+            this.txtDosya.TabIndex = 0;
+            // 
+            // btnSec
+            // 
+            this.btnSec.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSec.AutoSize = true;
+            this.btnSec.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.btnSec.Location = new System.Drawing.Point(543, 40);
+            this.btnSec.Name = "btnSec";
+            this.btnSec.Size = new System.Drawing.Size(103, 31);
+            this.btnSec.TabIndex = 1;
+            this.btnSec.Text = "Select .sql";
+            this.btnSec.UseVisualStyleBackColor = true;
+            this.btnSec.Click += new System.EventHandler(this.btnSec_Click);
+            // 
+            // btnAktar
+            // 
+            this.btnAktar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAktar.AutoSize = true;
+            this.btnAktar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.btnAktar.Location = new System.Drawing.Point(12, 444);
+            this.btnAktar.Name = "btnAktar";
+            this.btnAktar.Size = new System.Drawing.Size(671, 52);
+            this.btnAktar.TabIndex = 3;
+            this.btnAktar.Text = "Aktarımı Başlat";
+            this.btnAktar.UseVisualStyleBackColor = true;
+            this.btnAktar.Click += new System.EventHandler(this.btnAktar_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(12, 502);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(671, 23);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // txtServer
+            // 
+            this.txtServer.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtServer.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.txtServer.Location = new System.Drawing.Point(21, 52);
+            this.txtServer.Name = "txtServer";
+            this.txtServer.Size = new System.Drawing.Size(625, 31);
+            this.txtServer.TabIndex = 5;
+            // 
+            // txtCatalog
+            // 
+            this.txtCatalog.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtCatalog.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.txtCatalog.Location = new System.Drawing.Point(21, 112);
+            this.txtCatalog.Name = "txtCatalog";
+            this.txtCatalog.Size = new System.Drawing.Size(625, 31);
+            this.txtCatalog.TabIndex = 6;
+            // 
+            // txtUserID
+            // 
+            this.txtUserID.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtUserID.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.txtUserID.Location = new System.Drawing.Point(21, 167);
+            this.txtUserID.Name = "txtUserID";
+            this.txtUserID.Size = new System.Drawing.Size(625, 31);
+            this.txtUserID.TabIndex = 7;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.txtPassword.Location = new System.Drawing.Point(21, 222);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(625, 31);
+            this.txtPassword.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.label1.Location = new System.Drawing.Point(18, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 18);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "SQL Server";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.label2.Location = new System.Drawing.Point(18, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 18);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Initial Catalog";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.label3.Location = new System.Drawing.Point(18, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 18);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "User ID";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.label4.Location = new System.Drawing.Point(18, 201);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 18);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Password";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtServer);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtCatalog);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtUserID);
+            this.groupBox1.Controls.Add(this.txtPassword);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(671, 273);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Aktarılacak Sunucu Bilgileri";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.txtDosya);
+            this.groupBox2.Controls.Add(this.btnSec);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.groupBox2.Location = new System.Drawing.Point(12, 309);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(671, 96);
+            this.groupBox2.TabIndex = 14;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Aktarılacak Generate Script";
+            // 
+            // frm1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(695, 537);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnAktar);
+            this.Name = "frm1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = ".frm Run";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDosya;
+        private System.Windows.Forms.Button btnSec;
+        private System.Windows.Forms.Button btnAktar;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox txtServer;
+        private System.Windows.Forms.TextBox txtCatalog;
+        private System.Windows.Forms.TextBox txtUserID;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ SQLScriptRun/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Data.SqlClient;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+
+namespace SQLScriptRun
+{
+    public partial class frm1 : Form
+    {
+        public frm1()
+        {
+            InitializeComponent();
+        }
+        //public SqlConnection baglanti = new SqlConnection("Data Source=31.210.35.26;Initial Catalog=z_KonyaSeker;User ID=konyaseker_user;Password=53l1pTdNLSQeFVI;");
+        //SqlConnection baglanti = new SqlConnection(@"Data Source=85.25.196.237\MSSQLSERVER2008;Initial Catalog=z_KonyaSeker;User ID=u_KonyaSeker;Password=p_KonyaSeker;");    
+        void VeritabanıKurulumu()
+        {
+            SqlConnection baglanti = new SqlConnection(@"Data Source=" + txtServer.Text + ";Initial Catalog=" + txtCatalog.Text + ";User ID=" + txtUserID.Text + ";Password=" + txtPassword.Text + ";");
+            string dosya = File.ReadAllText(txtDosya.Text);
+            string[] komutlar = Regex.Split(dosya, @"^\s*GO\s*$", RegexOptions.Multiline | RegexOptions.IgnoreCase);
+            baglanti.Open();
+            progressBar1.Maximum = komutlar.Length;
+            bool sonuc = true;
+            foreach (string komut in komutlar)
+            {
+                if (komut.Trim() != "")
+                {
+                    try
+                    {
+                        new SqlCommand(komut, baglanti).ExecuteNonQuery();
+                        progressBar1.Value++;
+                    }
+                    catch
+                    {
+                        //sonuc = false;
+                    }
+                }
+            }
+            if (sonuc)
+                MessageBox.Show("Script Başarıyla Çalıştırıldı.", "Başarılı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            baglanti.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            progressBar1.Minimum = 0;
+
+        }
+
+        private void btnSec_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ac = new OpenFileDialog();
+            ac.Filter = "SQL Script (*.sql) |*.sql";
+            ac.Multiselect = false;
+            if (ac.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                txtDosya.Text = ac.FileName;
+            else
+                txtDosya.Text = "";
+
+        }
+
+        private void btnAktar_Click(object sender, EventArgs e)
+        {
+            progressBar1.Value++;
+            Thread t = new Thread(new ThreadStart(VeritabanıKurulumu));
+            t.Start();
+
+        }
+ 
+
+
+    }
+}
